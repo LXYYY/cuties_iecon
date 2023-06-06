160317ad274f54164f66cd85e21137809ed7c58b
Mia-99 <329208364@qq.com>
2023-06-06 11:45:06 +0800

    Update unicycleDT.m via Euler method

--- unicycleDT.m
+++ unicycleDT.m
@@ -1,5 +1,14 @@
 function xk1 = unicycleDT(xk, uk,Ts)
-    xk1 = [xk(1) + Ts*uk(1)*cos(xk(3));...
-        xk(2) + Ts*uk(1)*sin(xk(3));...
-        xk(3) + Ts*uk(2)];
+M=10;
+delta = Ts/M;
+xk1 = xk;
+% Repeat application of Euler method sampled at Ts/M.
+for ct=1:M
+    xk1 = xk1 + delta*unicycleCT(xk1,uk);
+end
+xk1 = xk;
+
+        % xk1 = xk1 + delta*[xk1(1) + Ts*uk(1)*cos(xk1(3));...
+        %         xk1(2) + Ts*uk(1)*sin(xk1(3));...
+        %         xk1(3) + Ts*uk(2)];
 end
